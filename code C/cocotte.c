35c79f5d31dde62f249f6f18e9526fdb179d2afb
Ele-rigal <riwgal2@gmail.com>
2018-02-28 14:56:55 +0100

    Rename cocotte.c to code C/cocotte.c

--- code C/cocotte.c
+++ code C/cocotte.c
@@ -1,13 +1,13 @@
-/* pour compiler ce fichier:  gcc –o cocotte cocotte.c libgraphique.c –lglut –lGLU –lGL –lm */
+/* pour compiler ce fichier:  gcc â€“o cocotte cocotte.c libgraphique.c â€“lglut â€“lGLU â€“lGL â€“lm */
 
-/* indispensable pour compiler un programme utilisant la bibliothèque graphique: */
+/* indispensable pour compiler un programme utilisant la bibliothÃ¨que graphique: */
 #include "libgraphique.h"
 
-/* ces points sont les sommets d'une ligne polygonale représentant une cocotte en papier: */
+/* ces points sont les sommets d'une ligne polygonale reprÃ©sentant une cocotte en papier: */
 #define  NB_POINTS 9
 int points[NB_POINTS][2] = { { 1, 3 }, { 0, 2 }, { 0, 0 }, { 2, 2 }, { 2, 4 }, { 0, 4 }, { 4, 0 }, { 2, 0 }, { 1, 1 } };
 
-/* WINDOW_WIDTH et WINDOW_HEIGHT sont deux pseudo-constantes définies dans libgraphique.h
+/* WINDOW_WIDTH et WINDOW_HEIGHT sont deux pseudo-constantes dÃ©finies dans libgraphique.h
    qui expriment respectivement la largeur et la hauteur de la zone graphique.
    COTE_MIN sera la plus petite de ces deux dimensions */
 #define COTE_MIN (WINDOW_WIDTH < WINDOW_HEIGHT ? WINDOW_WIDTH : WINDOW_HEIGHT)
@@ -15,16 +15,16 @@ int points[NB_POINTS][2] = { { 1, 3 }, { 0, 2 }, { 0, 0 }, { 2, 2 }, { 2, 4 }, {
 int main() {
     int i, xOrig, yOrig, xExtr, yExtr;
 
-    /* un peu d'espace pour que le dessin ne soit pas collé aux bords de la zone graphique: */
+    /* un peu d'espace pour que le dessin ne soit pas collÃ© aux bords de la zone graphique: */
     int marge = 50;
 
-    /* constante de proportionnalité entre les coordonnées graphiques et celles des points ci-dessus: */
+    /* constante de proportionnalitÃ© entre les coordonnÃ©es graphiques et celles des points ci-dessus: */
     int rapport = (COTE_MIN - 2 * marge) / 4;
 
-    /* en commençant il faut "allumer" le mode graphique */
+    /* en commenÃ§ant il faut "allumer" le mode graphique */
     start_graphics();
 
-    /* pour tracer des lignes et écrire des textes on choisit la couleur noire */
+    /* pour tracer des lignes et Ã©crire des textes on choisit la couleur noire */
     set_drawing_color(color_BLACK);
 
     /* chaque tour de cette boucle trace un segment joignant les points (xOrig, yOrig) et (xExtr, yExtr) */
@@ -40,17 +40,17 @@ int main() {
         yOrig = yExtr;
     }
 
-    /* on écrit un texte */
+    /* on Ã©crit un texte */
     draw_string(9 * COTE_MIN / 16, 3 * COTE_MIN / 4, "Pour terminer, pressez une touche");
 
-    /* on provoque l'affichage effectif découlant des opérations précédentes
-       (qui, jusqu'ici, n'étaient que "virtuelles") */
+    /* on provoque l'affichage effectif dÃ©coulant des opÃ©rations prÃ©cÃ©dentes
+       (qui, jusqu'ici, n'Ã©taient que "virtuelles") */
     update_graphics();
 
     /* on attend la frappe d'une touche (quelconque) afin que l'utilisateur ait le temps de voir le dessin */
     get_key();
 
-    /* on "éteint" le mode graphique */
+    /* on "Ã©teint" le mode graphique */
     stop_graphics();
 
     return 0;
