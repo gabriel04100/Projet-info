98ff21f5f240c55f58b7982b1ab443cf2f351ea2
gabriel04100 <36797166+gabriel04100@users.noreply.github.com>
2018-03-14 20:58:10 +0100

    Add files via upload

---
+++ fonctions.c
@@ -0,0 +1,23 @@
+typedef struct
+{
+
+	int longueur;
+	int largeur;
+	int espacement;
+	
+
+
+
+}QUADRILLAGE;
+// definition de la structure de quadrillage dépendantes de 3 variables longueur largeur et l'espacement entre deux lignes/deux colonnes
+
+void draw_quadri(int longueur ,int largeur,int espacement)
+	{
+
+
+
+
+
+
+	}
+//construit un quadrillage graphique en fonction des données entrées ce fait en fonction de l'écran 
