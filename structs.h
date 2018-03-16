e017df6146cb1b94a8be07a8d2df0534a9171d77
gabriel04100 <36797166+gabriel04100@users.noreply.github.com>
2018-03-16 16:25:24 +0100

    Add files via upload

---
+++ structs.h
@@ -0,0 +1,27 @@
+typedef struct
+{
+
+	int lines;
+	int columns;
+	int size;
+	
+
+
+
+}GRID;
+typedef struct
+	{
+
+
+
+	int x;
+	int y;
+	}POS;
+typedef struct
+	{
+		int x;
+		int y;
+
+
+
+	}END;
