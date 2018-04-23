41153a48801fdc272f6283901f4e8b8c87ab03a5
gabriel04100 <36797166+gabriel04100@users.noreply.github.com>
2018-04-23 14:50:02 +0200

    Create lib.h

---
+++ lib.h
@@ -0,0 +1,9 @@
+typedef int ENTIER;
+ENTIER x;
+int dir();
+void sup_g(int li,int co,int t);
+void sup_d(int li,int co,int t);
+void sup_haut(int li,int co,int t);
+void sup_bas(int li,int co,int t);
+long factorielle(int n);
+void ini_graph();
